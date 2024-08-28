1494cea6d5dda31d4233c3466a266f0d0a7029a5
Samuele Castaldo <castaldo.samuele@gmail.com>
2024-08-28 15:54:52 +0200

    CommitEse

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
-import HomePage from './pages/HomePage';
-import Profile from './pages/Profile';
-import Settings from './pages/Settings';
-import Search from './pages/Search';
-import DetailsFilm from './pages/DetailsFilm';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import "./App.css";
+import HomePage from "./pages/HomePage";
+import Profile from "./pages/Profile";
+import Settings from "./pages/Settings";
+import Search from "./pages/Search";
+import DetailsFilm from "./pages/DetailsFilm";
+import Error from "./pages/Error";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 //extra extra ho creato la pagina search dei film
 //extra extra hover riproduci con i dettagli del film
@@ -12,15 +13,17 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-        <Router>
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/profile" element={<Profile />} />
-                <Route path="/settings" element={<Settings />} />
-                <Route path="/search" element={<Search />} />
-                <Route path="/detailsFilm" element={<DetailsFilm />} /> {/**Questa non è completata */}
-            </Routes>
-        </Router>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/detailsFilm/:movieId" element={<DetailsFilm />} />{" "}
+          {/**Questa non è completata */}
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
