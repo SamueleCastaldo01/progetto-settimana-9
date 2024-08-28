3b7b8dd1d96d1811b754dc21762da4a5b361147f
Samuele Castaldo <castaldo.samuele@gmail.com>
2024-08-28 16:42:56 +0200

    commit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,23 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 function NavBar() {
-    return(
-        <>
-        <nav
+  const location = useLocation();
+  return (
+    <>
+      <nav
         className="navbar navbar-expand-lg position-sticky top-0 z-3"
         data-bs-theme="dark"
-        style={{backgroundColor: "#221f1f"}}
+        style={{ backgroundColor: "#221f1f" }}
       >
         <div className="container-fluid">
-        <Link className="navbar-brand" to="/">
-            <img src="assets/img/netflix_logo.png" style={{ width: "100px", height: "55px" }} alt="Logo" />
-        </Link>
+          <Link className="navbar-brand" to="/">
+            <img
+              src="assets/img/netflix_logo.png"
+              style={{ width: "100px", height: "55px" }}
+              alt="Logo"
+            />
+          </Link>
           <button
             className="navbar-toggler"
             type="button"
@@ -26,34 +32,51 @@ function NavBar() {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active fw-bold" href="/">Home</a>
+                <a className="nav-link active fw-bold" href="/">
+                  Home
+                </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link fw-bold" href="#">TV Shows</a>
+                <a className="nav-link fw-bold" href="#">
+                  TV Shows
+                </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link fw-bold" href="#">Movies</a>
+                <a className="nav-link fw-bold" href="#">
+                  Movies
+                </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link fw-bold" href="/profile">Profile</a>
+                <a className="nav-link fw-bold" href="/profile">
+                  Profile
+                </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link fw-bold" href="/settings">Settings</a>
+                <a className="nav-link fw-bold" href="/settings">
+                  Settings
+                </a>
               </li>
             </ul>
             <div className="d-flex align-items-center">
-            <Link className="navbar-brand" to="/search">
-              <i className="bi bi-search icons"></i>
-            </Link>
-              <div id="kids" className="fw-bold">KIDS</div>
+              <Link className="navbar-brand" to="/search">
+                <i className="bi bi-search icons"></i>
+              </Link>
+              {location.pathname === "/" && (
+                <div id="kids" className="fw-bold me-3">
+                  cerca serie Tv
+                </div>
+              )}
+              <div id="kids" className="fw-bold">
+                KIDS
+              </div>
               <i className="bi bi-bell icons"></i>
               <i className="bi bi-person-circle icons"></i>
             </div>
           </div>
         </div>
       </nav>
-        </>
-    )
+    </>
+  );
 }
 
 export default NavBar;
