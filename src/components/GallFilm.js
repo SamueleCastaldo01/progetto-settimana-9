b54d975c50490fbb0179c3ef6d2b8244c67cbaba
Samuele Castaldo <castaldo.samuele@gmail.com>
2024-08-28 16:06:35 +0200

    commit

--- src/components/GallFilm.js
+++ src/components/GallFilm.js
@@ -1,46 +1,64 @@
 import React, { Component } from "react";
-import Button from '@mui/material/Button';
-import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import Button from "@mui/material/Button";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 
-class GallFilm extends Component {
-  state = {
-    isHovered: false,
+function GallFilm(props) {
+  const [isHovered, setIsHovered] = useState(false);
+  const navigate = useNavigate();
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
   };
 
-  handleMouseEnter = () => {
-    this.setState({ isHovered: true });
+  const handleMouseLeave = () => {
+    setIsHovered(false);
   };
 
-  handleMouseLeave = () => {
-    this.setState({ isHovered: false });
+  const handleRiproduci = (imdbID) => {
+    navigate("/detailsFilm/" + imdbID);
   };
 
-  render() {
-    const { src, title, year, type } = this.props;
-    const { isHovered } = this.state;
-
-    return (
-      <div 
-        className="col mb-4 text-center px-1"
-        onMouseEnter={this.handleMouseEnter}
-        onMouseLeave={this.handleMouseLeave}
-      >
-        <div className={`GallFilmCont ${isHovered ? 'hovered' : ''}`}>
-          <img className={`poster-img ${isHovered ? 'special-hover' : ''}`} src={src} alt="movie picture" />
-          {isHovered && (
-            <div className="film-details p-2 text-start position-absolute w-100" style={{backgroundColor: "black"}}>
-              <p className="mb-0 fw-bold">{title}</p>
-              <div className="text-center">
-                <Button className="w-100 bg-secondary" variant="contained" startIcon={<PlayArrowIcon />}>Riproduci</Button>
-              </div>
-              <p className="m-0 mt-2"><b>Anno:</b> {year}</p>
-              <p className="m-0 mt-2"><b>Tipo:</b> {type}</p>
+  return (
+    <div
+      className="col mb-4 text-center px-1"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <div className={`GallFilmCont ${isHovered ? "hovered" : ""}`}>
+        <img
+          className={`poster-img ${isHovered ? "special-hover" : ""}`}
+          src={props.src}
+          alt="movie picture"
+        />
+        {isHovered && (
+          <div
+            className="film-details p-2 text-start position-absolute w-100"
+            style={{ backgroundColor: "black" }}
+          >
+            <p className="mb-0 fw-bold">{props.title}</p>
+            <div className="text-center">
+              <Button
+                onClick={() => handleRiproduci(props.imdbID)}
+                className="w-100 bg-secondary"
+                variant="contained"
+                startIcon={<PlayArrowIcon />}
+              >
+                Riproduci
+              </Button>
             </div>
-          )}
-        </div>
+            <p className="m-0 mt-2">
+              <b>Anno:</b> {props.year}
+            </p>
+            <p className="m-0 mt-2">
+              <b>Tipo:</b> {props.type}
+            </p>
+          </div>
+        )}
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default GallFilm;
--- src/components/Gallerie.js
+++ src/components/Gallerie.js
@@ -4,71 +4,71 @@ import { CircularProgress } from "@mui/material";
 import { Alert } from "react-bootstrap";
 
 class Gallerie extends Component {
+  state = {
+    films: [],
+    isLoading: true,
+    isError: false,
+  };
 
-    state = {
-        films: [],
-        isLoading: true,
-        isError: false,
-      }
+  fetchFilms = () => {
+    fetch("http://www.omdbapi.com/?apikey=184dbc60&s=" + this.props.saga)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("La chiamata non è andata a buon fine");
+        }
+      })
+      .then((data) => {
+        // Usa "data" per rappresentare il risultato JSON
+        console.log("FILMS RECUPERATI DAL SERVER", data);
+        this.setState({
+          films: data.Search || [], // Imposta films con l'array "Search" o un array vuoto se non esiste
+          isLoading: false,
+        });
+      })
+      .catch((err) => {
+        console.log("ERRORE NEL RECUPERO DATI (internet)?", err);
+        this.setState({
+          isLoading: false,
+          isError: true,
+        });
+      });
+  };
 
+  componentDidMount() {
+    this.fetchFilms();
+  }
 
-      fetchFilms = () => {
-        fetch("http://www.omdbapi.com/?apikey=184dbc60&s=" + this.props.saga)
-          .then((response) => {
-            if (response.ok) {
-              return response.json();
-            } else {
-              throw new Error('La chiamata non è andata a buon fine');
-            }
-          })
-          .then((data) => { // Usa "data" per rappresentare il risultato JSON
-            console.log('FILMS RECUPERATI DAL SERVER', data);
-            this.setState({
-              films: data.Search || [], // Imposta films con l'array "Search" o un array vuoto se non esiste
-              isLoading: false,
-            });
-          })
-          .catch((err) => {
-            console.log('ERRORE NEL RECUPERO DATI (internet)?', err);
-            this.setState({
-              isLoading: false,
-              isError: true,
-            });
-          });
-      }
+  render() {
+    console.log(this.state.films);
 
-      componentDidMount() {
-        this.fetchFilms(); 
-      }
-
-    render() {
-        console.log(this.state.films)
-
-        return(
-            <>
+    return (
+      <>
         <h4 className="text-white text-start">{this.props.title}</h4>
         <div className="row row-cols-1 row-cols-sm-2 row-cols-lg-4 row-cols-xl-6 mb-1">
+          {this.state.isLoading && <CircularProgress color="inherit" />}
 
-        {this.state.isLoading &&
-            <CircularProgress color="inherit"/> 
-        }
+          {this.state.isError && (
+            <Alert variant="danger">Oops! Qualcosa è andato storto 😱</Alert>
+          )}
 
-        {this.state.isError && (
-                <Alert variant="danger">
-                  Oops! Qualcosa è andato storto 😱
-                </Alert>     
-            )}
-
-            {this.state.films.slice(0, 6).map((film, i) => {
-                return (
-                    <GallFilm key={i} src={film.Poster} title={film.Title} type={film.Type} year={film.Year}/>
-                );
-            })}
-
+          {this.state.films.slice(0, 6).map((film, i) => {
+            return (
+              <GallFilm
+                key={i}
+                src={film.Poster}
+                title={film.Title}
+                type={film.Type}
+                year={film.Year}
+                imdbID={film.imdbID}
+              />
+            );
+          })}
         </div>
-            </>
-        )
-    }
+      </>
+    );
+  }
 }
 
 export default Gallerie;
